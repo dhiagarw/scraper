7940344ef573d3bfcd9f270a9384df3ecbd288a4
Dhiraj Agarwal <89119667+dhiagarw@users.noreply.github.com>
2025-01-28 00:33:13 +0530

    Update assets.js

--- assets.js
+++ assets.js
@@ -1,44 +1,60 @@
 /**
  * assets.js
- * Update the list of URLs to scrape on Line 56
+ * Update the list of URLs to in pages.txt file
  * To run this script, copy and paste `node assets.js` in the terminal
  */
 
-const cheerio = require('cheerio');
+const { JSDOM } = require('jsdom');
 const fs = require('fs');
 const path = require('path');
+const XLSX = require("xlsx");
 
 
 // File paths for storing data
-const jsonFilePath = path.join(__dirname, 'assets.json');
+const xlsxFilePath = path.join(__dirname, 'Assets.xlsx');
 
-function saveToJSON(data) {
-   fs.writeFileSync(jsonFilePath, JSON.stringify(data, null, 2));
-   console.log(`Data saved`);
+function saveToExcel(data) {
+  const headers = Object.keys(data[0]);
+  const rows = data.map(item => {
+    return headers.map(key => {
+      const value = item[key];
+      if (Array.isArray(value)) {
+        return value.join('\n');
+      }
+      return value;
+    });
+  });
+  const ws_data = [headers, ...rows];
+  const ws = XLSX.utils.aoa_to_sheet(ws_data);
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+  XLSX.writeFile(wb, xlsxFilePath);
+  console.log('Data Saved');
 }
 
 async function fetchData(url) {
     const response = await fetch(url);
-    const $ = cheerio.load(await response.text());
+    const htmlContent = await response.text();
+    const dom = new JSDOM(htmlContent);
+    const document = dom.window.document;
     let data = {};
     let xlsx = [];
     let pdfs = [];
     let images = [];
 
-    const country = url.split('/')[3];
-
-    $('a').each((index, element) => {
-        const anchorsrc = $(element).attr('href');
-        if (anchorsrc?.includes('.xlsx')){
-            xlsx.push(anchorsrc);
+    document.querySelectorAll('a').forEach(anchor => {
+        const href = anchor.href;
+        if (href?.includes('.xlsx')){
+            xlsx.push(href);
         }
-        else if (anchorsrc?.includes('.pdf')){
-            pdfs.push(anchorsrc);
+        else if (href?.includes('.pdf')){
+            pdfs.push(href);
         }
     });
 
-    $('img').each((index, element) => {
-        const imgsrc = $(element).attr('src');
+    document.querySelectorAll('img').forEach(img => {
+        const imgsrc = img.src;
         images.push(imgsrc);
     });
 
@@ -49,14 +65,18 @@ async function fetchData(url) {
     return data;
 }
 
+function getUrls() {
+  const data = fs.readFileSync('pages.txt', 'utf8');
+  return data.split('\n');
+}
+
 (async () => {
-   const urls = ["https://www.sample.com/abc.html","https://www.sample.com/def.html"];
+   const urls = getUrls();
 
    let allData = [];
    for (const url of urls) {
          data = await fetchData(url);
          allData = allData.concat(data);
    }
-   saveToJSON(allData);
-
+   saveToExcel(allData);
 })();
